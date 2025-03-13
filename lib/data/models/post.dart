9b2094953a2fb1bd51e664e89da8d2b94c03e32a
prxsss <9phuriphat.k@gmail.com>
2025-03-13 22:23:13 +0700

    refactor: simplify Post model by removing unused fields

--- lib/data/models/post.dart
+++ lib/data/models/post.dart
@@ -3,25 +3,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Post {
   String title;
   String content;
-  List<String> tags;
-  String category;
   Timestamp createdAt;
   Timestamp updatedAt;
-  int likesCount;
-  int viewsCount;
-  int commentsCount;
   String imageUrl;
 
   Post({
     required this.title,
     required this.content,
-    required this.tags,
-    required this.category,
     required this.createdAt,
     required this.updatedAt,
-    required this.likesCount,
-    required this.viewsCount,
-    required this.commentsCount,
     required this.imageUrl,
   });
 
@@ -29,13 +19,8 @@ class Post {
     : this(
         title: json['title']! as String,
         content: json['content']! as String,
-        tags: json['tags']! as List<String>,
-        category: json['category']! as String,
         createdAt: json['createdAt']! as Timestamp,
         updatedAt: json['updatedAt']! as Timestamp,
-        likesCount: json['likesCount']! as int,
-        viewsCount: json['viewsCount']! as int,
-        commentsCount: json['commentsCount']! as int,
         imageUrl: json['imageUrl']! as String,
       );
 
@@ -54,13 +39,8 @@ class Post {
     return Post(
       title: title ?? this.title,
       content: content ?? this.content,
-      tags: tags ?? this.tags,
-      category: category ?? this.category,
       createdAt: createdAt ?? this.createdAt,
       updatedAt: updatedAt ?? this.updatedAt,
-      likesCount: likesCount ?? this.likesCount,
-      viewsCount: viewsCount ?? this.viewsCount,
-      commentsCount: commentsCount ?? this.commentsCount,
       imageUrl: imageUrl ?? this.imageUrl,
     );
   }
@@ -69,13 +49,8 @@ class Post {
     return {
       'title': title,
       'content': content,
-      'tags': tags,
-      'category': category,
       'createdAt': createdAt,
       'updatedAt': updatedAt,
-      'likesCount': likesCount,
-      'viewsCount': viewsCount,
-      'commentsCount': commentsCount,
       'imageUrl': imageUrl,
     };
   }
