13f304a4b75267e0be6f834edb3eeaa92502ab98
prxsss <9phuriphat.k@gmail.com>
2025-03-15 02:10:27 +0700

    feat: add user management functionality to DatabaseService

--- lib/data/services/database_service.dart
+++ lib/data/services/database_service.dart
@@ -1,12 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crafted/data/models/post.dart';
+import 'package:crafted/data/models/user.dart';
 
 const String POST_COLLECTION_REF = 'posts';
+const String USER_COLLECTION_REF = 'users';
 
 class DatabaseService {
   final _firestore = FirebaseFirestore.instance;
 
   late final CollectionReference _postsRef;
+  late final CollectionReference _usersRef;
 
   DatabaseService() {
     _postsRef = _firestore
@@ -15,13 +18,25 @@ class DatabaseService {
           fromFirestore: (snapshots, _) => Post.fromJson(snapshots.data()!),
           toFirestore: (post, _) => post.toJson(),
         );
+    _usersRef = _firestore
+        .collection(USER_COLLECTION_REF)
+        .withConverter<User>(
+          fromFirestore: (snapshots, _) => User.fromJson(snapshots.data()!),
+          toFirestore: (user, _) => user.toJson(),
+        );
   }
 
+  // posts
   Stream<QuerySnapshot> getPosts() {
     return _postsRef.snapshots();
   }
 
   void addPost(Post post) async {
     await _postsRef.add(post);
+  }
+
+  // users
+  void saveUser(User user) async {
+    await _usersRef.doc(user.email).set(user);
   }
 }
