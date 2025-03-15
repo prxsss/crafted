27899a8b1d6b23478a68deaf835d9bbc69644664
prxsss <9phuriphat.k@gmail.com>
2025-03-15 22:57:28 +0700

    feat: initialize Supabase in main application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:crafted/presentation/screens/auth_wrapper.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: Color(0xffd08700));
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Supabase.initialize(
+    url: 'https://zgrzmkizoqasamisnbho.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpncnpta2l6b3Fhc2FtaXNuYmhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNTI1NTEsImV4cCI6MjA1NzYyODU1MX0.r0bSaDa9vSYMUSAPwj4DmUMOjG0JzVvPzmBicei3etM',
+  );
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );
